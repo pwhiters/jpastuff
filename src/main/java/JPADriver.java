53e9dc03a7c111cee44398d2996dde73615313e2
Phillip Whiters <phillip.whiters@weather.com>
2016-02-12 16:47:22 -0500

    Updated HSQLDB

--- src/main/java/JPADriver.java
+++ src/main/java/JPADriver.java
@@ -1,5 +1,36 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
 /**
- * Created by phillip.whiters on 2/12/16.
+ * Created by pwhiters on 1/28/2016.
  */
 public class JPADriver {
+
+    public static void main(String[] args){
+
+        Person pr = new Person();
+        pr.setPersonName("Mat Whiters");
+        //pr.setPersonId(12345);
+
+//        EntityManagerFactory entityManagerFactory; // = Persistence.createEntityManagerFactory("PersonPU");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersonPU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = null;
+
+        try {
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            entityManager.persist(pr);
+            entityTransaction.commit();
+        }
+        catch(RuntimeException e){
+            if(entityTransaction.isActive())
+                entityTransaction.rollback();
+            throw e;
+        }
+
+    }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,31 @@
-/**
- * Created by phillip.whiters on 2/12/16.
- */
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="Person")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Person_ID")
+    private int personId;
+
+    @Column(name = "Person_Name")
+    private String personName;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+//    public void setPersonId(int personId) {
+//        this.personId = personId;
+//    }
 }
